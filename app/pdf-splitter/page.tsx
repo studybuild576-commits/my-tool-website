7daf040664f82a4e5095c6bea975e8681e41fd19
studybuild576-commits <studybuild576@gmail.com>
2025-10-19 08:42:55 +0530

    Create page.tsx

---
+++ app/pdf-splitter/page.tsx
@@ -0,0 +1,9 @@
+import PDFSplitterTool from "@/components/PDFSplitterTool";
+
+export default function Page() {
+  return (
+    <main className="max-w-3xl mx-auto px-4 py-10">
+      <PDFSplitterTool />
+    </main>
+  );
+}
