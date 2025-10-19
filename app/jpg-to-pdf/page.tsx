50418082098e44e590361674340df6aebd8df0ad
studybuild576-commits <studybuild576@gmail.com>
2025-10-19 08:34:37 +0530

    Create page.tsx

---
+++ app/jpg-to-pdf/page.tsx
@@ -0,0 +1,9 @@
+import JPGToPDFTool from "@/components/JPGToPDFTool";
+
+export default function Page() {
+  return (
+    <main className="max-w-3xl mx-auto px-4 py-10">
+      <JPGToPDFTool />
+    </main>
+  );
+}
