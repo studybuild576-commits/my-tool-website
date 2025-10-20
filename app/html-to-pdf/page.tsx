a83de6acd82cab1ee60452d6e6f1d8dc75c526e2
studybuild576-commits <studybuild576@gmail.com>
2025-10-20 14:55:40 +0530

    Create page.tsx

---
+++ app/html-to-pdf/page.tsx
@@ -0,0 +1,8 @@
+import HTMLToPDFTool from "@/components/HTMLToPDFTool";
+export default function Page() {
+  return (
+    <main className="max-w-3xl mx-auto px-4 py-10">
+      <HTMLToPDFTool />
+    </main>
+  );
+}
