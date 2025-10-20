06b92009e7cb24c486856b5fd91c96536e1fa6b1
studybuild576-commits <studybuild576@gmail.com>
2025-10-20 14:54:27 +0530

    Create page.tsx

---
+++ app/pdf-organize/page.tsx
@@ -0,0 +1,8 @@
+import PDFOrganizeTool from "@/components/PDFOrganizeTool";
+export default function Page() {
+  return (
+    <main className="max-w-3xl mx-auto px-4 py-10">
+      <PDFOrganizeTool />
+    </main>
+  );
+}
