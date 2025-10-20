75a431b375d3bd266a630a442c4360661ebcdbfb
studybuild576-commits <studybuild576@gmail.com>
2025-10-20 14:53:47 +0530

    Create page.tsx

---
+++ app/pdf-rotate/page.tsx
@@ -0,0 +1,8 @@
+import PDFRotateTool from "@/components/PDFRotateTool";
+export default function Page() {
+  return (
+    <main className="max-w-3xl mx-auto px-4 py-10">
+      <PDFRotateTool />
+    </main>
+  );
+}
