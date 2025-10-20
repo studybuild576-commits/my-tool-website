4a9fd8cc8385865d72326ecc99114a4cf20a95bc
studybuild576-commits <studybuild576@gmail.com>
2025-10-20 07:06:33 +0530

    Create page.tsx

---
+++ app/pdf-reader/page.tsx
@@ -0,0 +1,8 @@
+import PDFReaderTool from "@/components/PDFReaderTool";
+export default function Page() {
+  return (
+    <main className="max-w-3xl mx-auto px-4 py-10">
+      <PDFReaderTool />
+    </main>
+  );
+}
