32fe6ab5925312a4c536f9f09d8b712f88fb0f42
studybuild576-commits <studybuild576@gmail.com>
2025-10-29 13:02:03 +0530

    Create PDFReaderPageClient.tsx

---
+++ components/client/PDFReaderPageClient.tsx
@@ -0,0 +1,5 @@
+// components/client/PDFReaderPageClient.tsx
+"use client";
+import dynamic from "next/dynamic";
+const PDFReaderTool = dynamic(() => import("@/components/PDFReaderTool"), { ssr: false });
+export default function PDFReaderPageClient() { return <PDFReaderTool />; }
