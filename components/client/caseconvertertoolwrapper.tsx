0a2612929a0627dee167a342762ac83d8648eb9c
studybuild576-commits <studybuild576@gmail.com>
2025-10-31 07:47:49 +0530

    Delete components/client/caseconvertertoolwrapper.tsx

--- components/client/caseconvertertoolwrapper.tsx
+++
@@ -1,4 +0,0 @@
-"use client";
-import dynamic from "next/dynamic";
-const CaseConverterTool = dynamic(() => import("@/components/CaseConverterTool"), { ssr: false });
-export default function CaseConverterToolWrapper() { return <CaseConverterTool />; }
