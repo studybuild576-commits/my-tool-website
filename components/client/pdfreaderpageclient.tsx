bf6db70dcf29d417b7dfe441d1faa59f7d0b3d98
studybuild576-commits <studybuild576@gmail.com>
2025-10-31 07:52:29 +0530

    Delete components/client/pdfreaderpageclient.tsx

--- components/client/pdfreaderpageclient.tsx
+++
@@ -1,4 +0,0 @@
-"use client";
-import dynamic from "next/dynamic";
-const PDFReaderTool = dynamic(() => import("@/components/PDFReaderTool"), { ssr: false });
-export default function PDFReaderPageClient() { return <PDFReaderTool />; }
